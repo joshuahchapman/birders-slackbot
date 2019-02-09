d4217e92c92700369cdd1269bd0b2c8d3559da26
Josh Chapman <joshuahchapman@users.noreply.github.com>
2019-02-09 10:47:46 -0800

    break commands into separate module

---
+++ commands.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+
+
+def recent(slack_client, ebird_client, cmd_params, to_channel_id):
+
+    lat = cmd_params.pop(0)
+    long = cmd_params.pop(0)
+
+    print('lat={lat}, long={long}'.format(lat=lat, long=long))
+
+    for param in cmd_params:
+        print('parsing parameter: ' + param)
+
+    df = ebird_client.get_recent_observations_by_lat_long(lat, long, distance=8, days_back=3)
+
+    print('Rows returned: {rowcount}'.format(rowcount=len(df.index)))
+
+    if df.empty or 'errors' in df.columns:
+        return_message = 'eBird returned no observations near latitude ' + lat + ', longitude ' + long
+
+    else:
+        return_message = ''
+        for index, row in df.iterrows():
+            # Format the datetime nicely for display.
+            pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
+                '%-m/%-d at %-I:%M %p')
+            return_message = return_message + '*' + row['comName'] + '*, ' + \
+                row['locName'] + ', on ' + pretty_dtm + '\n'
+
+    print('Sending message to Slack (channel: {channel}): {msg}'.format(channel=to_channel_id, msg=return_message))
+
+    # send channel a message
+    channel_msg = slack_client.api_call(
+        "chat.postMessage",
+        channel=to_channel_id,
+        text=return_message
+    )
+
+    if channel_msg['ok']:
+        print('Message sent to Slack successfully')
+    else:
+        print('Error message from Slack: ' + channel_msg['error'])
+
+    return
+
+
+def recent_notable(slack_client, ebird_client, cmd_params, to_channel_id):
+
+    lat = cmd_params[0]
+    long = cmd_params[1]
+
+    print('lat={lat}, long={long}'.format(lat=lat, long=long))
+
+    df = ebird_client.get_recent_notable_observations_by_lat_long(lat, long, distance=8, days_back=3)
+
+    if df.empty or 'errors' in df.columns:
+        return_message = 'eBird returned no notable observations near latitude ' + lat + ', longitude ' + long
+
+    else:
+        return_message = ''
+        for index, row in df.iterrows():
+            # Format the datetime nicely for display.
+            pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
+                '%-m/%-d at %-I:%M %p')
+            return_message = return_message + '*' + row['comName'] + '*, ' + \
+                row['locName'] + ', on ' + pretty_dtm + '\n'
+
+    print('Sending message to Slack (channel: {channel}): {msg}'.format(channel=to_channel_id, msg=return_message))
+
+    # send channel a message
+    channel_msg = slack_client.api_call(
+        "chat.postMessage",
+        channel=to_channel_id,
+        text=return_message
+    )
+
+    if channel_msg['ok']:
+        print('Message sent to Slack successfully')
+    else:
+        print('Error message from Slack: ' + channel_msg['error'])
+
+    return
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from threading import Thread
 from flask import Flask, request, make_response
 from slackclient import SlackClient
 from ebird import EbirdClient
+import commands
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 EBIRD_TOKEN = os.environ["EBIRD_TOKEN"]
@@ -38,6 +39,9 @@ def parse_parameters(parameter_list):
         valid = True
         validation_message = 'Command received. Working on it!'
 
+    # temporary alias until users convert to new name
+    cmd = 'recent_notable' if cmd == 'recent-notable' else cmd
+
     return valid, validation_message, cmd, parameter_list
 
 
@@ -45,84 +49,10 @@ def handle_command(cmd, cmd_params, to_channel_id):
 
     print('Handling command...')
 
-    if cmd == 'recent':
+    func = getattr(commands, cmd)
+    func(slack_client, ebird_client, cmd_params, to_channel_id)
 
-        lat = cmd_params.pop(0)
-        long = cmd_params.pop(0)
-
-        print('lat={lat}, long={long}'.format(lat=lat, long=long))
-
-        for param in cmd_params:
-            print('parsing parameter: ' + param)
-
-        df = ebird_client.get_recent_observations_by_lat_long(lat, long, distance=8, days_back=3)
-
-        print('Rows returned: {rowcount}'.format(rowcount=len(df.index)))
-
-        if df.empty or 'errors' in df.columns:
-            return_message = 'eBird returned no observations near latitude ' + lat + ', longitude ' + long
-
-        else:
-            return_message = ''
-            for index, row in df.iterrows():
-                # Format the datetime nicely for display.
-                pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
-                    '%-m/%-d at %-I:%M %p')
-                return_message = return_message + '*' + row['comName'] + '*, ' + \
-                    row['locName'] + ', on ' + pretty_dtm + '\n'
-
-        print('Sending message to Slack (channel: {channel}): {msg}'.format(channel=to_channel_id, msg=return_message))
-
-        # send channel a message
-        channel_msg = slack_client.api_call(
-            "chat.postMessage",
-            channel=to_channel_id,
-            text=return_message
-        )
-
-        if channel_msg['ok']:
-            print('Message sent to Slack successfully')
-        else:
-            print('Error message from Slack: ' + channel_msg['error'])
-
-        return
-
-    if cmd == 'recent-notable':
-
-        lat = cmd_params[0]
-        long = cmd_params[1]
-
-        print('lat={lat}, long={long}'.format(lat=lat, long=long))
-
-        df = ebird_client.get_recent_notable_observations_by_lat_long(lat, long, distance=8, days_back=3)
-
-        if df.empty or 'errors' in df.columns:
-            return_message = 'eBird returned no notable observations near latitude ' + lat + ', longitude ' + long
-
-        else:
-            return_message = ''
-            for index, row in df.iterrows():
-                # Format the datetime nicely for display.
-                pretty_dtm = datetime.strptime(row['obsDt'], '%Y-%m-%d %H:%M').strftime(
-                    '%-m/%-d at %-I:%M %p')
-                return_message = return_message + '*' + row['comName'] + '*, ' + \
-                    row['locName'] + ', on ' + pretty_dtm + '\n'
-
-        print('Sending message to Slack (channel: {channel}): {msg}'.format(channel=to_channel_id, msg=return_message))
-
-        # send channel a message
-        channel_msg = slack_client.api_call(
-            "chat.postMessage",
-            channel=to_channel_id,
-            text=return_message
-        )
-
-        if channel_msg['ok']:
-            print('Message sent to Slack successfully')
-        else:
-            print('Error message from Slack: ' + channel_msg['error'])
-
-        return
+    return
 
 
 @app.route("/slack/ebird", methods=["POST"])
