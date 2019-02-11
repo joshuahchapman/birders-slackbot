09b6b6f5dd2829cad4e78cb2fafb8917e50b0c7e
Josh Chapman <joshuahchapman@users.noreply.github.com>
2019-02-10 22:53:40 -0800

    move ebird command parsing to its proper module

--- ebird_commands.py
+++ ebird_commands.py
@@ -1,6 +1,39 @@
 from datetime import datetime
 
 
+ROOT_CMD = '5mr'
+
+# list of accepted commands and their required parameters
+COMMAND_PARAMS = {
+    'recent': ['latitude', 'longitude'],
+    'recent_notable': ['latitude', 'longitude']
+}
+
+
+def parse_parameters(parameter_list):
+
+    cmd = parameter_list.pop(0)
+
+    # temporary alias until users convert to new name
+    cmd = 'recent_notable' if cmd == 'recent-notable' else cmd
+
+    if cmd not in COMMAND_PARAMS.keys():
+        valid = False
+        validation_message = 'Sorry, I don''t recognize the command _' + cmd + '_. ' + \
+            'These are the commands I know: _' + '_, _'.join(COMMAND_PARAMS.keys()) + '_.'
+
+    elif len(parameter_list) < len(COMMAND_PARAMS[cmd]):
+        valid = False
+        validation_message = 'Looks like you don''t have enough inputs.\n' \
+            + 'Expected format: `/ebird ' + cmd + ' ' + ' '.join(COMMAND_PARAMS[cmd]) + '`.'
+
+    else:
+        valid = True
+        validation_message = 'Command received. Working on it!'
+
+    return valid, validation_message, cmd, parameter_list
+
+
 def recent(slack_client, ebird_client, cmd_params, to_channel_id):
 
     lat = cmd_params.pop(0)
--- server.py
+++ server.py
@@ -14,46 +14,6 @@ ebird_client = EbirdClient(EBIRD_TOKEN)
 
 app = Flask(__name__)
 
-# list of accepted commands and their required parameters
-COMMAND_PARAMS = {
-    'recent': ['latitude', 'longitude'],
-    'recent_notable': ['latitude', 'longitude']
-}
-
-
-def parse_parameters(parameter_list):
-
-    cmd = parameter_list.pop(0)
-
-    # temporary alias until users convert to new name
-    cmd = 'recent_notable' if cmd == 'recent-notable' else cmd
-
-    if cmd not in COMMAND_PARAMS.keys():
-        valid = False
-        validation_message = 'Sorry, I don''t recognize the command _' + cmd + '_. ' + \
-            'These are the commands I know: _' + '_, _'.join(COMMAND_PARAMS.keys()) + '_.'
-
-    elif len(parameter_list) < len(COMMAND_PARAMS[cmd]):
-        valid = False
-        validation_message = 'Looks like you don''t have enough inputs.\n' \
-            + 'Expected format: `/ebird ' + cmd + ' ' + ' '.join(COMMAND_PARAMS[cmd]) + '`.'
-
-    else:
-        valid = True
-        validation_message = 'Command received. Working on it!'
-
-    return valid, validation_message, cmd, parameter_list
-
-
-def handle_command(cmd, cmd_params, to_channel_id):
-
-    print('Handling command...')
-
-    func = getattr(ebird_commands, cmd)
-    func(slack_client, ebird_client, cmd_params, to_channel_id)
-
-    return
-
 
 @app.route("/slack/ebird", methods=["POST"])
 def ebird_command():
@@ -68,13 +28,14 @@ def ebird_command():
     print(cmd)
 
     # Validate parameters
-    params_valid, validation_message, cmd, cmd_parameters = parse_parameters(full_command)
+    params_valid, validation_message, cmd, cmd_parameters = ebird_commands.parse_parameters(full_command)
 
     if not params_valid:
         return make_response(validation_message, 200)
 
     else:
-        thread = Thread(target=handle_command, args=(cmd, cmd_parameters, user_id))
+        func = getattr(ebird_commands, cmd)
+        thread = Thread(target=func, args=(slack_client, ebird_client, cmd_parameters, user_id))
         thread.start()
 
         return make_response(validation_message, 200)
